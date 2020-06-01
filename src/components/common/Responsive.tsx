0ce719e722bc2669fe4fcc8ce0543d92de5b7a88
오민섭 <ominseob@ominseob-ui-MacBookPro.local>
2020-06-02 01:12:59 +0900

    add @types/recoil

--- src/components/common/Responsive.tsx
+++ src/components/common/Responsive.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 interface ResponsiveProps {}
 const Responsive: React.FC<ResponsiveProps> = ({ children, ...rest }) => {
